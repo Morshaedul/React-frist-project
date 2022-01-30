6d9da9f263b356f155ce05fe1984bf7984f3d1e0
Morshaedul <morshaedulanirbanit@gmail.com>
2022-01-30 19:39:20 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Tajin from './Tajin';
+import Header from './Header';
+import About from './About';
+import Menu from './Menu';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      
+      <Tajin></Tajin>
+      <Header></Header>
+      <About></About>
+      <Menu></Menu>
+      <Content></Content>
+      <Footer></Footer>
+
+    </div>
+    
+    
+  );
+}
+
+export default App;
---
+++ src/Mor.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Mor(){
+    return(
+        <div> 
+            <h1>Morshaedul </h1>
+            <Mor></Mor>
+            </div>
+        
+    )
+}
+export default Mor;
